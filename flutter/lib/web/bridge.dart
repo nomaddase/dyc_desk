77e5b033bec0342909a137a0ecbb2e5ddbd9d7b8
nomaddase <67481591+nomaddase@users.noreply.github.com>
2025-11-05 20:45:16 +0500

    Add stubbed Rustdesk bridge for analyzer

--- flutter/lib/web/bridge.dart
+++ flutter/lib/web/bridge.dart
@@ -7,48 +7,7 @@ import 'package:uuid/uuid.dart';
 import 'dart:html' as html;
 
 import 'package:flutter_hbb/consts.dart';
-
-final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
-
-mixin _$EventToUI {
-  Object get field0 => throw _privateConstructorUsedError;
-}
-
-sealed class EventToUI {
-  const factory EventToUI.event(
-    String field0,
-  ) = EventToUI_Event;
-  const factory EventToUI.rgba(
-    int field0,
-  ) = EventToUI_Rgba;
-  const factory EventToUI.texture(
-    int field0,
-    bool field1,
-  ) = EventToUI_Texture;
-}
-
-class EventToUI_Event implements EventToUI {
-  const EventToUI_Event(final String field0) : this.field = field0;
-  final String field;
-  String get field0 => field;
-}
-
-class EventToUI_Rgba implements EventToUI {
-  const EventToUI_Rgba(final int field0) : field = field0;
-  final int field;
-  int get field0 => field;
-}
-
-class EventToUI_Texture implements EventToUI {
-  const EventToUI_Texture(final int field0, final bool field1)
-      : f0 = field0,
-        f1 = field1;
-  final int f0;
-  final bool f1;
-  int get field0 => f0;
-  bool get field1 => f1;
-}
+import 'package:flutter_hbb/models/event_to_ui.dart';
 
 class RustdeskImpl {
   Future<void> stopGlobalEventStream({required String appType, dynamic hint}) {
